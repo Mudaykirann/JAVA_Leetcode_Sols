4b6cb13231db3aa22e06d6b9aa80998361df731b
UDAY KIRAN <123919244+Mudaykirann@users.noreply.github.com>
2024-08-26 00:00:20 +0530

    Time: 62 ms (97.97%) | Memory: 49 MB (73.67%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -3,19 +3,30 @@ class Solution {
         if (n <= 2) {
             return 0;
         }
+
+        // Create a boolean array "isPrime" and initialize
+        // all entries as true. A value in isPrime[i] will
+        // finally be false if i is Not a prime, true otherwise.
         boolean[] isPrime = new boolean[n];
-        for (int i = 2; i < n; i++) {
-            isPrime[i] = true;
-        }
-        for (int i = 2; i * i < n; i++) {
+        Arrays.fill(isPrime, true);
+
+        // We know that 0 and 1 are not prime numbers
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        // Only check odd numbers starting from 3, skip even numbers
+        for (int i = 3; i * i < n; i += 2) {
             if (isPrime[i]) {
-                for (int j = i * i; j < n; j += i) {
+                // Mark all odd multiples of i, starting from i*i
+                for (int j = i * i; j < n; j += 2 * i) {
                     isPrime[j] = false;
                 }
             }
         }
-        int count = 0;
-        for (int i = 2; i < n; i++) {
+
+        // Count primes, considering 2 separately
+        int count = 1; // since 2 is prime
+        for (int i = 3; i < n; i += 2) {
             if (isPrime[i]) {
                 count++;
             }
