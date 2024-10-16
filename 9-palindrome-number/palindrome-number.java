e0a1d793b154c19df702849a23f73e4054c55b30
UDAY KIRAN <123919244+Mudaykirann@users.noreply.github.com>
2024-10-16 22:22:31 +0530

    Time: 8 ms (10.25%) | Memory: 44 MB (49.86%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;  
+        
+        String str = String.valueOf(x);  
+        StringBuilder sb = new StringBuilder(str).reverse();  
+        
+        return str.equals(sb.toString());
+    }
+}
